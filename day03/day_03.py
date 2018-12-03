8adff42b20545b9db0cbb6a0dfe42339b695883f
Unknown <bonnjona@hotmail.fr>
2018-12-03 20:48:13 +0100

    day 3 solved

---
+++ day03/day_03.py
@@ -0,0 +1,35 @@
+import re
+from abstract_day import AbstractDay
+
+
+class Day03First(AbstractDay):
+
+    @staticmethod
+    def check_elf_claims(matrix, claims):
+        for elf_claim in claims:
+            parts = re.findall('#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', elf_claim)
+            parts = list(map(int, list(parts[0])))
+            for i in range(parts[2] + parts[4]):
+                if len(matrix) <= i:
+                    matrix.append([[set(), 0] for _ in range(parts[1] + parts[3])])
+                if i in range(parts[2], parts[2] + parts[4] + 1):
+                    for j in range(parts[1] + parts[3]):
+                        if len(matrix[i]) <= j:
+                            matrix[i].append([set(), 0])
+                        if j in range(parts[1], parts[1] + parts[3] + 1):
+                            matrix[i][j][0].add(parts[0])
+                            matrix[i][j][1] += 1
+
+    def get_result(self):
+        matrix = list()
+        self.check_elf_claims(matrix, self.input_content)
+        return sum([1 for line in matrix for cell in line if cell[1] > 1])
+
+
+class Day03Second(Day03First):
+
+    def get_result(self):
+        matrix = list()
+        self.check_elf_claims(matrix, self.input_content)
+        overlaps = set().union(*[set(cell[0]) for line in matrix for cell in line if cell[1] > 1])
+        return set().union(*[cell[0] for line in matrix for cell in line if cell[1] == 1 and not (cell[0] & overlaps)])
---
+++ day03/test_day_03.py
@@ -0,0 +1,23 @@
+from day03.day_03 import Day03First, Day03Second
+
+
+class TestDay03First:
+
+    def test_day03_first_ex1(self):
+        input_data = [x.strip() for x in open('input_ex1.txt').readlines()]
+        assert Day03First(input_data).get_result() == 4
+
+    def test_day03_first_final(self):
+        input_data = [x.strip() for x in open('input.txt').readlines()]
+        print(Day03First(input_data).get_result())
+
+
+class TestDay03Second:
+
+    def test_day03_second_ex1(self):
+        input_data = [x.strip() for x in open('input_ex1.txt').readlines()]
+        assert Day03Second(input_data).get_result() == {3}
+
+    def test_day03_second_final(self):
+        input_data = [x.strip() for x in open('input.txt').readlines()]
+        print(Day03Second(input_data).get_result())
